3f8a2416a8abdad4bda5e60182ad58c373bf26b6
Paper <nites0262@gmail.com>
2025-11-28 18:46:50 +0530

    Added DocType JSON definitions

---
+++ sync_app/doctype/sync_transaction_log/test_sync_transaction_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Apstic and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSyncTransactionLog(FrappeTestCase):
+	pass
